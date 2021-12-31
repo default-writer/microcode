7c07aa5d2aa3822409619e405e54dd5aafe63506
Artur Mustafin <funcelot@outlook.com>
2021-12-31 10:12:46 +0000

    fix script

--- tools/cmake.sh
+++ tools/cmake.sh
@@ -6,7 +6,7 @@ apt upgrade -y
 apt remove '^cmake.*' -y --purge
 apt-get install build-essential libtool autoconf unzip wget libssl-dev -y
 apt autoremove -y
-[ !-d "$PWD/temp" ] && mkdir $PWD/temp
+[ ! -d "$PWD/temp" ] && mkdir $PWD/temp
 cd $PWD/temp
 wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
 tar -xzvf cmake-$version.$build.tar.gz
