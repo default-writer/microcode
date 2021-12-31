5a65f9d43d7579482609c2fff538769c634210c6
Artur Mustafin <funcelot@outlook.com>
2021-12-31 09:25:18 +0000

    switched to new toolset

---
+++ tools/cmake.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+version=3.22
+build=1
+apt-get update -y
+apt upgrade -y
+apt remove '^cmake.*' -y --purge
+apt-get install build-essential libtool autoconf unzip wget libssl-dev -y
+apt autoremove -y
+[ !-d "$PWD/temp" ] && mkdir $PWD/temp
+cd $PWD/temp
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
+./bootstrap
+make -j$(nproc)
+make install
+[ -d "$PWD/temp" ] && rm -rf $PWD/temp
---
+++ tools/gcc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+version=3.22
+build=1
+apt-get update -y
+apt upgrade -y
+apt-get install gcc-11 g++-11 -y
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 110
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 110
+update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-11 110
+apt autoremove -y
